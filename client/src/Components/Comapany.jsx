278178ca286f64f90607dbca185467182497003b
sachidanandatripathy <sachidananda.tripathy03@gmail.com>
2023-10-24 00:09:06 +0530

    few other things added to company route

--- client/src/Components/Comapany.jsx
+++ client/src/Components/Comapany.jsx
@@ -1,6 +1,54 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Styles/Company.css';
 import cardData from './Json/Category.json';
+import companies from './Json/Companies.json'
+import Pagination from "react-js-pagination";
+
+function Companies() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const companiesPerPage = 7;
+    const indexOfLastCompany = currentPage * companiesPerPage;
+    const indexOfFirstCompany = indexOfLastCompany - companiesPerPage;
+    const currentCompanies = companies.slice(indexOfFirstCompany, indexOfLastCompany);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    return (
+        <>
+            <div className="companies">
+                {currentCompanies.map((company, index) => (
+                    <div key={index} className="company-card">
+                        <div className="company-card-header">
+                            <h3>{company.companyName}</h3>
+                        </div>
+                        <div className="company-card-body">
+                            <p>Review: 5</p>
+                            <p>Rating: nice</p>
+                            <button>View More</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="pagination">
+                <Pagination
+                    activePage={currentPage}
+                    itemsCountPerPage={companiesPerPage}
+                    totalItemsCount={companies.length}
+                    pageRangeDisplayed={7}
+                    onChange={handlePageChange}
+                    itemClass="page-item"
+                    linkClass="page-link"
+                    activeClass="active"
+                />
+            </div>
+        </>
+
+    );
+}
+
+
 
 function CompanyCategory() {
     const rowRef = useRef(null);
@@ -51,6 +99,9 @@ function Company() {
     return (
         <div>
             <CompanyCategory />
+            <h2 style={{display:"flex",justifyContent:"center",padding:"10px"}}>Companies Actively Hiring</h2>
+            <hr/>
+            <Companies />
         </div>
     );
 }
